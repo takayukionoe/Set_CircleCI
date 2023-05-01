5ebabe75efea835ddbdbd8e2dbe0e020dd200952
takayukionoe <100008521+takayukionoe@users.noreply.github.com>
2023-05-01 22:49:58 +0900

    ansible修正

--- serverspec/spec/localhost/sample_spec.rb
+++ serverspec/spec/localhost/sample_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
-listen_port = 80
-describe package('nginx') do
+
+describe package('git') do
   it { should be_installed }
-end
-describe port(listen_port) do
-  it { should be_listening }
-end
-describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
-  its(:stdout) { should match /^200$/ }
 end
